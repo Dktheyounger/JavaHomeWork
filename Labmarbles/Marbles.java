fff67c99f03f974a788e830d65027ff068930923
Daniel Kelsch <daniel.o.kelsch@gmail.com>
2020-01-22 15:31:12 -0800

    Realigned Output

--- Labmarbles/Marbles.java
+++ Labmarbles/Marbles.java
@@ -6,42 +6,47 @@ class Marbles
    {
       Scanner scanner = new Scanner(System.in);   
       
-      System.out.print("Enter color:");
+      System.out.print("color:");
       String color1 = scanner.nextLine();
       System.out.printf("Number of %s marbles:", color1);
       int number1 = scanner.nextInt();
       scanner.nextLine();
       
-      System.out.print("Enter color:");
+      System.out.print("color:");
       String color2 = scanner.nextLine();
       System.out.printf("Number of %s marbles:", color2);
       int number2 = scanner.nextInt();
       scanner.nextLine();
       
-      System.out.print("Enter color:");
+      System.out.print("color:");
       String color3 = scanner.nextLine();
       System.out.printf("Number of %s marbles:", color3);
       int number3 = scanner.nextInt();
       scanner.nextLine();
       
+      
+      System.out.println();
+      
       String colorHeader = "Color";
       String numberHeader = "Number of Marbles";
-      String dashes = "----------------";
+      String dashes1 = "---------";
+      String dashes2 = "-----------------";
+  
       
-      System.out.printf("%10s ", colorHeader);
-      System.out.printf("%10s %n", numberHeader);
+      System.out.printf("%-11s ", colorHeader);
+      System.out.printf("%-10s %n", numberHeader);
       
-      System.out.printf("%10s ", dashes);
-      System.out.printf("%10s %n", dashes);
+      System.out.printf("%-11s ", dashes1);
+      System.out.printf("%-10s %n", dashes2);
       
-      System.out.printf("%10s", color1);
-      System.out.printf("%10s %n", number1);
+      System.out.printf("%-12s", color1);
+      System.out.printf("%-10s %n", number1);
       
-      System.out.printf("%10s", color2);
-      System.out.printf("%10s %n", number2);
+      System.out.printf("%-12s", color2);
+      System.out.printf("%-10s %n", number2);
       
-      System.out.printf("%10s", color3);
-      System.out.printf("%10s %n", number3);
+      System.out.printf("%-12s", color3);
+      System.out.printf("%-10s %n", number3);
       
  
       
